59f40cabceaa3bfc1b7e411a34727b23cf9f242c
danieljohndavis <mcgoldrd@gmail.com>
2020-07-07 18:14:13 -0700

    Overall your assignment looks great. THe only note I had was to incorporate the time zone/offset into your tempAndTime object. I also eliminated some redundant variables. Great job.
    Tests look good and run well.

--- src/main/java/cst438/hw2/domain/TempAndTime.java
+++ src/main/java/cst438/hw2/domain/TempAndTime.java
@@ -15,16 +15,18 @@ public class TempAndTime {
 
   // Converts temp form Kelvin to Farenheit and returns new value
   public double ConvertTempToFahrenheit() {
-    double tempF = Math.round((temp - 273.15) * 9.0 / 5.0 + 32.0);
-    return tempF;
+    // Shortened this line up a little
+    return Math.round((temp - 273.15) * 9.0 / 5.0 + 32.0);
   }
 
-  // TODO: Set date to correct format
+
   // Converts the time into the correct string format and returns it
   public String convertTimeToString() {
+    // Where are you adding in the time zone and adjusting for time offset?
+    // Specifications request local time.
     Date date = new Date((this.time) * 1000);
-    String convertedTime = date.toString();
-    return convertedTime;
+    // Removed redundant variable here as well.
+    return date.toString();
   }
 
 }
